ab66b90e5f3478849704ad2e1eaa0ba1962e0bbf
mgonzales <mgonzalesh@gmail.com>
2012-08-09 13:08:56 -0500

    cambios controladora flickr

--- flickr/app/controllers/flickr_controller.rb
+++ flickr/app/controllers/flickr_controller.rb
@@ -1,3 +1,8 @@
 class FlickrController < ApplicationController
-	def
+	def search
+		FlickRaw.api_key = 'eddf035520f0271f7003c837d67a5cca'
+		photos = flickr.photos.search(:tags=>params[:tags], :per_page => 4)
+		@piece = render_to_string :partial => 'photo.text',
+			:collection => photos.map{ |photo| {:square => FlickRaw.url_s(photo), :big => FlickRaw.url(photo)}}
+	end
 end
